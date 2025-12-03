bb8a34c26a6b73b74d23bb9c35cd382c651725f4
Fainus Gabriel <nusgabriel17@gmail.com>
2025-12-03 12:32:49 +0300

    added components

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Vehicles from "./components/Vehicles";
+import About from "./components/About";
+import Reviews from "./components/Reviews";
+import Contact from "./components/Contact";
+import "./styles.css";
+
+function App() {
+  const [page, setPage] = useState("home");
+
+  return (
+    <div>
+      <Navbar setPage={setPage} />
+      {page === "home" && <Home />}
+      {page === "vehicles" && <Vehicles />}
+      {page === "about" && <About />}
+      {page === "reviews" && <Reviews />}
+      {page === "contact" && <Contact />}
+    </div>
+  );
+}
+
+export default App;
